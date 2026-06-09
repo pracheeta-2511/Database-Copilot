2fa79a10e7ff37b55a829091fc0b8bf56d5ad1b0
Pracheeta Parida <2330173@kiit.ac.in>
2026-06-10 02:17:22 +0530

    Update db_mcp_server.py

--- Database-Copilot/db_mcp_server.py
+++ Database-Copilot/db_mcp_server.py
@@ -5,10 +5,10 @@ mcp = FastMCP("Database_Expert")
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="inventory_db"
+        host="DB_HOST",
+        user="DB_USER",
+        password="DB_PASSWORD",
+        database="iDB_NAME"
     )
 
 @mcp.tool()
